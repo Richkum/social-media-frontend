4bbd5362ac50801f1eae023a2f7d9460bfbbaa7a
Richkum <kumatum2@gmail.com>
2024-10-01 16:17:26 +0100

    building authentication UI

--- social-media-client/src/components/landingPage/mainCont/MainContext.jsx.jsx
+++ social-media-client/src/components/landingPage/mainCont/MainContext.jsx.jsx
@@ -109,8 +109,8 @@ const MainContent = () => {
           </div>
         </div>
       </section>
-
-      {/* <section id="testimonials" className="py-20 bg-deepSkyBlue text-white">
+      {/* 
+      <section id="testimonials" className="py-20 bg-deepSkyBlue text-white">
         <div className="container mx-auto px-4">
           <h3 className="text-2xl md:text-3xl font-bold text-center mb-12">
             What Our Users Say
